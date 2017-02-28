78ada4e7799111e5dd02998f8ac2e8b5365c60f3
pakcharles <pakchar@hawaii.edu>
2017-02-27 23:29:51 -1000

    Task 5: Security with methods, Filtering data with publish and subscribe

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -11,6 +11,7 @@ import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
 
 
@@ -39,12 +40,7 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     target.text.value = '';
